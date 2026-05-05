f1575903520bc15cd4fe8f9542c1043fe49e589f
MDJoon <a01047768319@gmail.com>
2026-05-05 17:57:55 +0900

    sever-side support

--- src/main/java/com/mdjoon/mapdirection/mixin/MapStateMixin.java
+++ src/main/java/com/mdjoon/mapdirection/mixin/MapStateMixin.java
@@ -1,5 +1,9 @@
 package com.mdjoon.mapdirection.mixin;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MapRenderer;
+import net.minecraft.client.renderer.state.MapRenderState;
+import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.decoration.ItemFrame;
@@ -15,42 +19,44 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 import java.util.Optional;
 
-@Mixin(MapItemSavedData.class)
-public abstract class MapStateMixin {
-    @Final
+@Mixin(MapRenderer.class)
+public class MapStateMixin {
+
     @Shadow
-    private Map<String, net.minecraft.world.level.saveddata.maps.MapDecoration> decorations;
+    private TextureAtlas decorationSprites;
 
-    @Inject(method = "tickCarriedBy", at = @At("TAIL"))
-    private void showDirectionOutsideMap(
-            Player player, ItemStack itemStack, ItemFrame placedInFrame, CallbackInfo ci
-    ) {
-        if (player == null) return;
+    @Inject(
+            method = "extractDecorationRenderState",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void modifyDecoration(MapDecoration decoration, CallbackInfoReturnable<MapRenderState.MapDecorationRenderState> cir) {
+        var state = cir.getReturnValue();
 
-        String key = player.getName().getString();
-        MapDecoration decoration = decorations.get(key);if(decoration == null) return;
-        Holder<MapDecorationType> decoType = decoration.type();
+        var type = decoration.type();
 
-        if(decoType == MapDecorationTypes.PLAYER_OFF_MAP || decoType == MapDecorationTypes.PLAYER_OFF_LIMITS) {
-            byte x = decoration.x();
-            byte z = decoration.y();
-            byte rotation = (byte) ((player.getYRot() % 360 + 360) % 360 / 22.5);
-            decorations.remove(key);
+        if (type == MapDecorationTypes.PLAYER_OFF_MAP ||
+                type == MapDecorationTypes.PLAYER_OFF_LIMITS) {
 
-            decorations.put(
-                    key,
-                    new MapDecoration(
-                            MapDecorationTypes.PLAYER,
-                            x,
-                            z,
-                            rotation,
-                            Optional.of(Component.literal("Out of Map!"))
-                    )
+            state.atlasSprite = decorationSprites.getSprite(
+                    MapDecorationTypes.PLAYER.value().assetId()
             );
+            state.name = Component.literal("Out of Map!");
+            Player player = Minecraft.getInstance().player;
+
+            if (player != null) {
+                float yaw = player.getYRot();
+                yaw = (yaw % 360 + 360) % 360;
+
+                state.rot = (byte)(yaw / 360.0F * 16.0F);
+            }
         }
+
+        cir.setReturnValue(state);
     }
 }
